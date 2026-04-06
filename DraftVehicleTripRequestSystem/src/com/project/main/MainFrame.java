31aff82a4c07033090aa73966867cd9b5816cf0c
bulsu-slsabrajano <2024100842@ms.bulsu.edu.ph>
2026-04-06 17:29:57 +0800

    Create Draft Project

---
+++ DraftVehicleTripRequestSystem/src/com/project/main/MainFrame.java
@@ -0,0 +1,23 @@
+package com.project.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainFrame extends Application{
+
+	public MainFrame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
